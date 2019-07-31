22207b5765bd055362a0e5dc97e5400ff778c797
yikifp <martinsazzaroli@gmail.com>
2019-07-31 19:05:02 -0300

    new changes

--- 02_PolarCordinates/sketch.js
+++ 02_PolarCordinates/sketch.js
@@ -1,33 +1,48 @@
-var r, a, aVel, aAcc;
+var a, x, y, r, aAcc, aVel;
 function setup() {
-var canvas = createCanvas(600, 600);
-r = 100;
-a = 0.0;
-aVel = 0.00;
-aAcc = 1; 
-background(0)
+  var canvas = createCanvas(600, 600);
 
+  angleMode(DEGREES)
+  a = 45;
+  aAcc = .010;
+  aVel = 1
+  rr = getRandomValue(true, false)
 }
 
 function draw(){
-var x = r * cos(a);
-var y = r * sin(a);
-
-translate(width/2, height/2);
-noFill();
-stroke(255);
-
-
-  line(0,0, x, y);
-  ellipse(0,0, 50, 50);
-  ellipse(x, y, 20, 20); 
-
-
-
-
+ background(0);
+  aVel = constrain(aVel + aAcc, 0, 4);
+  
+  if(aVel >= 4 || aVel <= 1){
+  aAcc *= -1
+  }
+  
+  a = a + aVel
+  translate(width/2, height/2);
+  rotate(cos(a)*45)
+  circles()
 };
 
+function circles (){
+    for(var j = 0; j <= 400; j+=40 ) {
+      var r = j
+      var alpha = map(j, 0, 400, 0, 255)
+      var red = map(j, 0, 400, 0, 255)
 
+    for(var i = 0; i < 360; i+=10) {
+      x = r * cos(i);
+      y = r * sin(i);
+      if(i % 20){
+      fill(255, 0, 0, alpha)
+      ellipse(x, y, r*.10, r*.10);
+      } else {
+      fill(0, 0, 255, alpha) 
+      ellipse(x, y, r*.10, r*.10);
+      
+      }
+    }
+  }
+}
 
 function getRandomValue(a, b){
 
